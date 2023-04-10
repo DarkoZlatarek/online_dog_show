71de4865af6e95f38e48372d6cd7f6697e1c1c11
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-10 19:01:47 +0000

    Add Enter class to models.py in competition folder

--- competition/models.py
+++ competition/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryFields
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Enter(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    competitor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_enter')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name=post_likes)
