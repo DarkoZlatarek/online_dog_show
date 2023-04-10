9ada0bc4d9e63f92e4a132c8f2da94b7295d02b9
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-10 19:08:29 +0000

    Add Comment class to the models.py in competition

--- competition/models.py
+++ competition/models.py
@@ -6,6 +6,7 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Enter(models.Model):
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     competitor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_enter')
@@ -15,4 +16,30 @@ class Enter(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name=post_likes)
+    likes = models.ManyToManyField(User, related_name=post_likes, blank=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+    
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Enter, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def number_of_likes(self):
+        return f'Comment {self.body} by {self.name}'
