75dadd02b3e4f455fc7933b9a0377e31ea2304e1
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-10 19:41:58 +0000

    Update EnterAdmin class in admin.py

--- competition/admin.py
+++ competition/admin.py
@@ -6,5 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Enter)
 class EnterAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
-
