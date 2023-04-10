9cb1e9b9f38ea8f3baa6380dccc517571d846f42
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-10 20:04:56 +0000

    Add EntriesList class to competition/views.py

--- competition/views.py
+++ competition/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Enter
 
-# Create your views here.
+
+class EntriesList(genetic.ListView):
+
+    model = Enter
+    queryset = Enter.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
