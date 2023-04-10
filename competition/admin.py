0107a99a1cb98527df6436ced5435fdc77ef4ca9
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-10 19:28:35 +0000

    Add summernote for the content field

--- competition/admin.py
+++ competition/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Enter
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Enter)
+
+@admin.register(Enter)
+class EnterAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
