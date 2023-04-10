e0b44e679199e1dae0fcdca624983601e71cad7f
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-10 19:55:30 +0000

    Add CommentAdmin class to admin.py

--- competition/admin.py
+++ competition/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Enter
+from .models import Enter, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -8,6 +8,22 @@ class EnterAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on', 'approved')
     search_fields = ['title', 'content']
     summernote_fields = ('content')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
