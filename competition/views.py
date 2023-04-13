1be984e18f430f562111bf3826332f1ab3c60cec
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-13 20:04:53 +0000

    Add EntryDetail class in competition/views.py

--- competition/views.py
+++ competition/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Enter
 
 
@@ -23,3 +23,24 @@ class EntriesList(generic.ListView):
     queryset = Enter.objects.filter(status=1).order_by('-created_on')
     template_name = 'competition.html'
     paginate_by = 6
+
+
+class EntryDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Entry.objects.filter(status=1)
+        entry = get_object_or_404(queryset, slug=slug)
+        comments = entry.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if entry.liked.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            'entry_detail.html',
+            {
+                'entry': entry,
+                'comments': comments,
+                'liked': liked
+            }
+        )
