1602707e2194efa5f8f5301e751409655d86cbe7
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-13 19:16:53 +0000

    Add about and competition html files

--- competition/urls.py
+++ competition/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.EntriesList.as_view(), name='home')
+    path('', views.home_page, name='home'),
+    path('about/', views.about_page, name='about'),
+    path('competition/', views.EntriesList.as_view(), name='competition')
 ]
--- competition/views.py
+++ competition/views.py
@@ -3,9 +3,23 @@ from django.views import generic
 from .models import Enter
 
 
+def home_page(request):
+    """
+    View for home page.
+    """
+    return render(request, 'index.html')
+
+
+def about_page(request):
+    """
+    View for about page.
+    """
+    return render(request, 'about.html')
+
+
 class EntriesList(generic.ListView):
 
     model = Enter
     queryset = Enter.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'competition.html'
     paginate_by = 6
