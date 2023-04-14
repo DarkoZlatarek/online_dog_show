1f307509503677b2b8b58d0e328e36bf3a859abe
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-14 20:02:44 +0000

    Update urls and settings for allauth

--- dogshow/urls.py
+++ dogshow/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('competition.urls'), name='competition_urls'),
+    path('accounts/', include('allauth.urls')),
 
 ]
