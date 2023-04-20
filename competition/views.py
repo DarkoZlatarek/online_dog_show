cccedb9ee9350d495f0b2832e539c1ca68149686
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-20 21:35:23 +0000

    Update EntryDetail class in views.py

--- competition/views.py
+++ competition/views.py
@@ -42,6 +42,38 @@ class EntryDetail(View):
             {
                 'entry': entry,
                 'comments': comments,
+                'commented': False,
+                'liked': liked,
+                'comment_form': CommentForm()
+            }
+        )
+    
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Enter.objects.filter(status=1)
+        entry = get_object_or_404(queryset, slug=slug)
+        comments = entry.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if entry.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = entry
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        
+        return render(
+            request,
+            'entry_detail.html',
+            {
+                'entry': entry,
+                'comments': comments,
+                'commented': True,
                 'liked': liked,
                 'comment_form': CommentForm()
             }
