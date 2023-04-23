dffdc9938de058f39903c8630d05a5f3319480c8
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-23 20:47:17 +0000

    Add path for enter page

--- competition/urls.py
+++ competition/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('about/', views.about_page, name='about'),
+    path('enter/', views.enter_submit, name='enter'),
     path('competition/', views.EntriesList.as_view(), name='competition'),
     path('<slug:slug>/', views.EntryDetail.as_view(), name='entry_detail'),
     path('like/<slug:slug>', views.EntryLike.as_view(), name='entry_like')
