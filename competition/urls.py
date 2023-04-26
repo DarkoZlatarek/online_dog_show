a2546db697d032d402d273438ea9208f66162de6
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-26 20:41:29 +0000

    Add delete path to urls.py

--- competition/urls.py
+++ competition/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('about/', views.about_page, name='about'),
     path('enter/', views.enter_submit, name='enter'),
     path('edit/<slug:slug>', views.edit_entry, name='edit_entry'),
+    path('delete/<slug:slug>', views.delete_entry, name='delete_entry'),
     path('competition/', views.EntriesList.as_view(), name='competition'),
     path('<slug:slug>/', views.EntryDetail.as_view(), name='entry_detail'),
     path('like/<slug:slug>', views.EntryLike.as_view(), name='entry_like')
