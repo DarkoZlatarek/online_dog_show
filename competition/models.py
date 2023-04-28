e66592c4a5653557c6eb70eecb0404651bac1b23
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-04-28 21:27:57 +0000

    Update models.py for Enter class

--- competition/models.py
+++ competition/models.py
@@ -1,25 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Enter(models.Model):
 
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(
+        max_length=200, unique=True, null=False, blank=False)
+    slug = models.SlugField(
+        max_length=200, unique=True, null=False, blank=False)
     competitor = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='post_enter')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
 
     class Meta:
         ordering = ['-created_on']
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Enter, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
