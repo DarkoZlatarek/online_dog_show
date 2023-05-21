c377f647fa687a1c93c6d3f626d02452a1d1b78c
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-05-21 21:02:09 +0000

    Add comments to form tests

--- competition/test_forms.py
+++ competition/test_forms.py
@@ -2,21 +2,33 @@ from django.test import TestCase
 from .forms import EnterForm, CommentForm
 
 
+# Test taken from "Hello Django"
 class TestEnterForm(TestCase):
-
+    """
+    Tests for Eter form.
+    """
     def test_title_is_required(self):
+        """
+        Verify title is required
+        """
         form = EnterForm({'title': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('title', form.errors.keys())
         self.assertEqual(form.errors['title'][0], 'This field is required.')
 
     def test_content_is_required(self):
+        """
+        Verify content is required
+        """
         form = EnterForm({'content': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('content', form.errors.keys())
         self.assertEqual(form.errors['content'][0], 'This field is required.')
 
     def test_featured_image_is_required(self):
+        """
+        Verify image is required
+        """
         form = EnterForm({'featured_image': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('featured_image', form.errors.keys())
@@ -24,6 +36,9 @@ class TestEnterForm(TestCase):
             form.errors['featured_image'][0], 'No file selected!')
 
     def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Verify fields are explicit
+        """
         form = EnterForm()
         self.assertEqual(
             form.Meta.fields, ('title', 'content', 'featured_image'))
@@ -32,6 +47,9 @@ class TestEnterForm(TestCase):
 class TestCommentForm(TestCase):
 
     def test_body_is_required(self):
+        """
+        Verify comment body is required
+        """
         form = CommentForm({'body': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('body', form.errors.keys())
