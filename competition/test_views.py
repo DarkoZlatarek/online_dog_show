a436dc6377b85ecbef5cf710606f51a70a9c0b5d
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-05-21 21:14:00 +0000

    Add comments to views tests

--- competition/test_views.py
+++ competition/test_views.py
@@ -2,24 +2,39 @@ from django.test import TestCase
 from .models import Enter
 
 
+# Test taken from "Hello Django"
 class TestViews(TestCase):
-
+    """
+    Tests for views.
+    """
     def test_get_home_page(self):
+        """
+        Verify home page loads
+        """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
-    def test_get_about_page(self):
-        response = self.client.get('/about', follow=True)
+    def test_get_rules_page(self):
+        """
+        Verify rules page loads
+        """
+        response = self.client.get('/rules', follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'about.html')
+        self.assertTemplateUsed(response, 'rules.html')
 
     def test_get_competition_page(self):
+        """
+        Verify competition page loads
+        """
         response = self.client.get('/competition', follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'competition.html')
 
     def test_get_login_page(self):
+        """
+        Verify login page loads
+        """
         response = self.client.get('/accounts/login', follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account/login.html')
