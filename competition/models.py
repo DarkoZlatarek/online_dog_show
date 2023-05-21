442a65f3572747aa12e817ce8db04b55dda76af5
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-05-21 20:25:18 +0000

    Add comments to models

--- competition/models.py
+++ competition/models.py
@@ -1,3 +1,7 @@
+"""
+Database models for the dogshow app
+& comments section.
+"""
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -6,8 +10,11 @@ from django.utils.text import slugify
 STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
+# Model used from "I think therefore I blog" walkthrough.
 class Enter(models.Model):
-
+    """
+    Database model for submitting an entry
+    """
     title = models.CharField(
         max_length=200, unique=True, null=False, blank=False)
     slug = models.SlugField(
@@ -22,21 +29,37 @@ class Enter(models.Model):
     likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
 
     class Meta:
+        """
+        To display entries in descending order
+        """
         ordering = ['-created_on']
 
     def save(self, *args, **kwargs):
+        """
+        Auto populate slug field
+        """
         self.slug = slugify(self.title)
         super(Enter, self).save(*args, **kwargs)
 
     def __str__(self):
+        """
+        To return a string representation of an object
+        """
         return self.title
 
     def number_of_likes(self):
+        """
+        Return total number of entry likes
+        """
         return self.likes.count()
 
 
+# Model used from "I think therefore I blog" walkthrough.
 class Comment(models.Model):
-
+    """
+    Database model for creating a comment
+    on an entry
+    """
     post = models.ForeignKey(
         Enter, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
@@ -46,7 +69,14 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
 
     class Meta:
+        """
+        Order comments in ascending order
+        """
         ordering = ['created_on']
 
     def __str__(self):
+        """
+        Display the comment as the comment text,
+        and comment author name
+        """
         return f'Comment {self.body} by {self.name}'
